ccba87d75b93718be89c4c2c0cf6a477c14626bb
RizkiBimo09 <80391679+RizkiBimo09@users.noreply.github.com>
2022-03-02 10:03:04 +0700

    Add files via upload

---
+++ test.php
@@ -0,0 +1,14 @@
+<?php
+
+  $nim = "20090028";
+  $nama = "Rizki Bimo Wijaya";
+  $kelas = "4B";
+    echo "My Biodata <br>
+    NIM   : $nim <br>
+    Nama  : $nama <br>
+    Kelas : $kelas <br>";
+
+ print("Semoga setelah mengenal kita bisa menyayangi sebab ada pepatah mengatakan 
+        - Tak kenal maka tak sayang -");
+  
+?>
